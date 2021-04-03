b9167cda68375e44ecfcd6ce2cd8aac368d67365
aaronshivers <42848750+aaronshivers@users.noreply.github.com>
2021-04-03 07:26:31 -0500

    test

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { StylesProvider } from '@material-ui/core/styles';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 const App = () => (
   <StylesProvider>
@@ -19,7 +19,6 @@ const App = () => (
         />
       </Switch>
     </BrowserRouter>
-
   </StylesProvider>
 );
 
